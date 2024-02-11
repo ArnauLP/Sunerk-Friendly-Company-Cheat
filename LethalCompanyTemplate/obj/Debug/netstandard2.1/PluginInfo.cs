5ec8435c665a5b478f330b0e7475a4719d6e177a
ArnauLP <arnaulopez004@outlook.com>
2024-02-11 01:51:03 +0100

    Updated Build Name (.dll)

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace LethalCompanyTemplate
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "LethalCompanyTemplate";
-        public const string PLUGIN_NAME = "LethalCompanyTemplate";
+        public const string PLUGIN_GUID = "Sunerk Friendly Company v0.0.1";
+        public const string PLUGIN_NAME = "Sunerk Friendly Company v0.0.1";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
