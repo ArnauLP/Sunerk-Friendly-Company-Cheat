b3b455b62e40dd28e1d3421eadac5a8f226f9973
ArnauLP <arnaulopez004@outlook.com>
2024-02-11 01:34:32 +0100

    Done README.md

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/LethalCompanyTemplate.AssemblyInfo.cs
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/LethalCompanyTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A template for Lethal Company")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec862ee318b300694bc8e5e50f3b415dbbfd5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9eaf406b252b503dc1d0175f2456f5cb19e51e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LethalCompanyTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LethalCompanyTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
